10c694d7f37b98b80db50f2630298322008d051d
Prem Patel <patelprem7736@gmail.com>
2023-07-28 18:19:30 +0530

    test

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 function Home() {
     return (
         <div className="h-screen w-screen flex items-center justify-center">
-            <div>"Throughout Heaven and Earth, I alone am honored"</div>
+            <div>"Hello World!!"</div>
         </div>
     );
 }
