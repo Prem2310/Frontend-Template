cce018461654f86510867a3f616901ec5e808b14
Janmejay Chatterjee <janmejaychatterjee@gmail.com>
2023-07-28 14:41:39 +0530

    Added Quote

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,9 @@
 function Home() {
-    return <div>Meow?</div>;
+    return (
+        <div className="h-screen w-screen">
+            <div>"Throughout Heaven and Earth, I alone am honored"</div>
+        </div>
+    );
 }
 
 export default Home;
